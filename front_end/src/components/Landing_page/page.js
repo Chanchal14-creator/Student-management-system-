c69f12cf52a94d90aeed7f242322e1afc3e3e4b3
Chanchal14-creator <chanchalbaghel60@gmail.com>
2024-03-04 14:09:41 +0530

    commit changes
    Signed-off-by: Chanchal14-creator <chanchalbaghel60@gmail.com>

--- front_end/src/components/Landing_page/page.js
+++ front_end/src/components/Landing_page/page.js
@@ -1,1 +1,22 @@
 import React from 'react'
+import './page.css'
+
+function Landing()
+{
+    return (
+        <div className='main'>
+            <div className='box'>
+                <div>
+
+                </div>
+                <div>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+export default Landing;
