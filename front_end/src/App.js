b506460511473600f3109bce44e209596dc0b5cd
Chanchal14-creator <chanchalbaghel60@gmail.com>
2024-03-06 14:38:22 +0530

    login form has been uploaded
    Signed-off-by: Chanchal14-creator <chanchalbaghel60@gmail.com>

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Landing from './components/Landing_page'
+import Landing from './components/Landing_page/page.js';
 
 function App() {
   return (
--- front_end/src/components/Landing_page/page.js
+++ front_end/src/components/Landing_page/page.js
@@ -1,18 +1,48 @@
-import React from 'react'
-import './page.css'
+import React, { useState } from 'react';
+import './page.css';
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
 
-function Landing()
-{
+
+
+function Landing() {
+    const [backgroundcolor, setbackgroundcolor] = useState(' rgba(134, 89, 89, 0.523)')
+    function colorchangehandler() {
+        setbackgroundcolor('white');
+    }
     return (
-        <div className='main'>
-            <div className='box'>
-                <div>
+        <div className="body">
+            <div className='nav1'>
+                <img src="./logo.png" />
+                <ul>
+                    <li>Home</li>
+                    <li>Contact us</li>
+                    <li>About us</li>
+                </ul>
+            </div>
 
+            <div className='box'>
+                <div >
+                    <div className='users'>
+                        <div style={{}} id="student"><a onclick={colorchangehandler} href="#">Student</a></div>
+                        <div style={{ backgroundcolor }} id="Faculty"><a onclick={colorchangehandler} href="#">Faculty</a></div>
+                    </div>
                 </div>
                 <div>
-                    <div>
-                        
+                    <div className='login_items'>
+                        <div className='input-box'><input type="text" placeholder="username" /><FaUser className='icon' /></div>
+                        <div className='input-box'><input type="text" placeholder="password" /><FaLock className='icon' /></div>
+                        <div className='flex-div'><div className='remember-forget'>
+                            <label><input type="checkbox" />Remember me</label>
+                            <a href="">forget password</a>
+                        </div>
+                        </div>
+                        <button id="login_button" type="submit">Login</button>
+                        <div className='register-link'>
+                            <p>Dont't have an account ?<a href="#">Register</a></p>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
