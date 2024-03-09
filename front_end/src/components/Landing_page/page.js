10b80bbf30639fec852e1f1f41eeb0e4ca3ae76d
Chanchal14-creator <chanchalbaghel60@gmail.com>
2024-03-09 11:14:56 +0530

    commit changes
    Signed-off-by: Chanchal14-creator <chanchalbaghel60@gmail.com>

--- front_end/src/components/Landing_page/page.js
+++ front_end/src/components/Landing_page/page.js
@@ -11,42 +11,31 @@ function Landing() {
         setbackgroundcolor('white');
     }
     return (
-        <div className="body">
-            <div className='nav1'>
-                <img src="./logo.png" />
-                <ul>
-                    <li>Home</li>
-                    <li>Contact us</li>
-                    <li>About us</li>
-                </ul>
-            </div>
-
-            <div className='box'>
-                <div >
-                    <div className='users'>
-                        <div style={{}} id="student"><a onclick={colorchangehandler} href="#">Student</a></div>
-                        <div style={{ backgroundcolor }} id="Faculty"><a onclick={colorchangehandler} href="#">Faculty</a></div>
-                    </div>
+        <div className='box'>
+            <div >
+                <div className='users'>
+                    <div style={{}} id="student"><a onclick={colorchangehandler} href="#">Student</a></div>
+                    <div style={{ backgroundcolor }} id="Faculty"><a onclick={colorchangehandler} href="#">Faculty</a></div>
                 </div>
-                <div>
-                    <div className='login_items'>
-                        <div className='input-box'><input type="text" placeholder="username" /><FaUser className='icon' /></div>
-                        <div className='input-box'><input type="text" placeholder="password" /><FaLock className='icon' /></div>
-                        <div className='flex-div'><div className='remember-forget'>
-                            <label><input type="checkbox" />Remember me</label>
-                            <a href="">forget password</a>
-                        </div>
-                        </div>
+            </div>
+            <div>
+                <div className='login_items'>
+                    <div className='input-box'><input type="text" placeholder="username" required/><FaUser className='icon' /></div>
+                    <div className='input-box'><input type="text" placeholder="password" required/><FaLock className='icon' /></div>
+                    <div className='remember-forget'>
+                        <div className='rm'><input type="checkbox" /><label>Remember me</label></div>
+                        <a href="">forget password</a>
+                    </div>
+
                         <button id="login_button" type="submit">Login</button>
                         <div className='register-link'>
                             <p>Dont't have an account ?<a href="#">Register</a></p>
                         </div>
-                    </div>
+                    
 
                 </div>
             </div>
         </div>
-
     );
 };
 export default Landing;
